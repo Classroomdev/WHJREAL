bec1b1afe1f1bcf6fb5870f3c6c0ed3783706097
Emmanuel Obonyo <emmyobonyo@gmail.com>
2025-02-13 10:34:42 +0300

    remove the profile option for now

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,38 +1,35 @@
-import React from 'react';
-import { Link, Routes, Route, Navigate } from 'react-router-dom';
-import Home from '../pages/Home';
-import Profile from '../pages/users/Profile';
-import GoogleOuathLogin from './GoogleOuathLogin';
-import InvalidUser from '../pages/users/InvalidUser';
-import CreateNewJob from '../pages/jobs/createNewJob';
-import UpdateJob from '../pages/jobs/updateJob';
-import JobsIndex from '../pages/jobs/JobsIndex';
-import Job from '../pages/jobs/Job';
+import React from "react";
+import { Link, Routes, Route, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import Profile from "../pages/users/Profile";
+import GoogleOuathLogin from "./GoogleOuathLogin";
+import InvalidUser from "../pages/users/InvalidUser";
+import CreateNewJob from "../pages/jobs/createNewJob";
+import UpdateJob from "../pages/jobs/updateJob";
+import JobsIndex from "../pages/jobs/JobsIndex";
+import Job from "../pages/jobs/Job";
 
 function Header() {
-  const UserId = localStorage.getItem('userId');
+  const UserId = localStorage.getItem("userId");
   const id = UserId;
   return (
     <>
-      <nav class='border-b'>
-        <div class='flex justify-between w-9/10 mx-auto items-center  my-3'>
-          <Link
-            to='/'
-            class='font-bold text-2xl'
-          >
+      <nav class="border-b">
+        <div class="flex justify-between w-9/10 mx-auto items-center  my-3">
+          <Link to="/" class="font-bold text-2xl">
             We Hire Juniors Also
           </Link>
-          <ul class='flex justify-between w-1/5 text-gray-600 text-lg'>
-            <li>
+          <ul class="flex justify-between w-1/5 text-gray-600 text-lg">
+            {/* <li>
               <Link to={`/users/${id}`}>Profile</Link>
-            </li>
+            </li> */}
             <li>
-              <Link to='/jobs/all'>Jobs</Link>
+              <Link to="/jobs/all">Jobs</Link>
             </li>
             <li>
               <Link
-                to='/login'
-                class='bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'
+                to="/login"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
               >
                 Sign Up
               </Link>
@@ -41,38 +38,14 @@ function Header() {
         </div>
       </nav>
       <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='/auth/google/callback'
-          element={<GoogleOuathLogin />}
-        />
-        <Route
-          path='/users/:id'
-          element={<Profile />}
-        />
-        <Route
-          path='/usernotfound'
-          element={<InvalidUser />}
-        />
-        <Route
-          path='/jobs/new'
-          element={<CreateNewJob />}
-        />
-        <Route
-          path='/jobs/:id'
-          element={<UpdateJob />}
-        />
-        <Route
-          path='/jobs/all'
-          element={<JobsIndex />}
-        />
-        <Route
-          path='/jobs/index/:id'
-          element={<Job />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/auth/google/callback" element={<GoogleOuathLogin />} />
+        <Route path="/users/:id" element={<Profile />} />
+        <Route path="/usernotfound" element={<InvalidUser />} />
+        <Route path="/jobs/new" element={<CreateNewJob />} />
+        <Route path="/jobs/:id" element={<UpdateJob />} />
+        <Route path="/jobs/all" element={<JobsIndex />} />
+        <Route path="/jobs/index/:id" element={<Job />} />
       </Routes>
     </>
   );

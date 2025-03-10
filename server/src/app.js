e9e116077258442a62e50e2f86aa75b4a80ef0e8
Emmanuel Obonyo <emmyobonyo@gmail.com>
2025-03-10 15:55:49 +0300

    Add render frontend url to the back access list

--- server/src/app.js
+++ server/src/app.js
@@ -1,31 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const express = require('express');
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-require('dotenv').config();
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const express = require("express");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+require("dotenv").config();
 
-const api = require('./routes/api');
-const authRoutes = require('./routes/auth');
-const passportSetup = require('./services/passport-setup');
+const api = require("./routes/api");
+const authRoutes = require("./routes/auth");
+const passportSetup = require("./services/passport-setup");
 
 const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ["http://localhost:3000", "https://whjreal.onrender.com/"],
   })
 );
 
 app.use(helmet());
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use(
   cookieSession({
@@ -37,12 +37,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/auth', authRoutes);
+app.use("/auth", authRoutes);
 
-app.use('/api', api);
+app.use("/api", api);
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 // app.get('/', (req, res) => {
 //   res.send('This is the home page');
